dea50a0aab256c46b56a148a8b32c7aa05fc4efe
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-29 14:11:17 +0530

    the faqs section

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { FaqsContainer } from './container/faqs';
 import { FooterContainer } from './container/footer';
 import { JumbotronContainer } from './container/jumbotron';
 
 
 function App() {
   return <><JumbotronContainer />
+  <FaqsContainer />
   <FooterContainer />
+  
   </>
   }
 export default App;
