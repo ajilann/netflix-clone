d061bd281dddc246b6c4bb4622e23fdcb05a2b28
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-30 13:26:42 +0530

    the netflix complete homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,14 +12,16 @@ export default function home() {
             <HeaderContainer>
                 <Feature>
                     <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
-                    <Feature.Subtitle>Watch anywhere. Cancel at any time.</Feature.Subtitle>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                    <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button> Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                    </OptForm>
+                
                 </Feature>
-            <OptForm>
-               <OptForm.Input placeholder="Email address" />
-               <OptForm.Button> Try it now</OptForm.Button>
-               <OptForm.Break />
-                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
-            </OptForm>
+            
             </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
