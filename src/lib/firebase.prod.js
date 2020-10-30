ddd2b294d5f20fa673b4289a46b1f90f3ae4b427
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-30 17:15:18 +0530

    the signin page is created

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,24 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+// we need to somehow seed teh database
+
+//we need a config here
+
+const config = {
+    apiKey: "AIzaSyB34WePJCYdwThBej-WRvpQsmOCtWX6YXQ",
+    authDomain: "netflix-clone-90e09.firebaseapp.com",
+    databaseURL: "https://netflix-clone-90e09.firebaseio.com",
+    projectId: "netflix-clone-90e09",
+    storageBucket: "netflix-clone-90e09.appspot.com",
+    messagingSenderId: "749712238048",
+    appId: "1:749712238048:web:251b00475d52cb080cb785"
+};
+
+const firebase = Firebase.initializeApp(config);
+
+//seedDatabase(firebase);
+
+export { firebase };
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,75 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { Form } from '../components';
+import { HeaderContainer } from '../container/header';
+import { FooterContainer } from '../container/footer';
+import * as ROUTES from '../constants/routes';
 
-export default function signup() {
-    return <p>Hello from the sign in!</p>;
+
+export default function SignIn() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+const [emailAddress, setEmailAddress] = useState('');
+const [password, setPassword] = useState('');
+const [error, setError] = useState('');
+
+
+    // check form input elements are valid
+
+    const isInvalid = password === '' || emailAddress === '';
+    
+    const handleSignIn = (event) => {
+        event.preventDefault();
+
+        //firebase work here.
+        return firebase 
+        .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
+  };
+
+    // email & password. 
+
+    return (
+    <><HeaderContainer>
+        <Form>
+            <Form.Title>Sign In</Form.Title>
+            {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+            <Form.Base onSubmit={handleSignIn} method="POST">
+                <Form.Input 
+                placeholder="Email address"
+                value={emailAddress}
+                onChange={({target}) => setEmailAddress(target.value)}
+                />
+                <Form.Input
+                placeholder="Password"
+                type="password"
+                autoComplete="off"
+                value={password}
+                onChange={({target}) => setPassword(target.value)}
+                />
+                <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
+                    Sign In
+                </Form.Submit>
+            </Form.Base>
+
+            <Form.Text>
+                New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+                This page is protected by Google reCAPTHA to ensure you're not a bot. Learn more. 
+            </Form.TextSmall>
+        </Form>
+        </HeaderContainer><FooterContainer />
+        </>
+    );
 }
