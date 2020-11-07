ffd99d927c8cb4a100e6a28a0851c48a3794a430
ajilann <ajilan.nazarudeen@gmail.com>
2020-11-07 20:37:46 +0530

    test the loading, profile, opt-form

--- src/__tests__/components/form.test.js
+++ src/__tests__/components/form.test.js
@@ -5,13 +5,12 @@ import { Form } from '../../components';
 jest.mock('react-router-dom');
 
 describe('<Form />', () => {
-    it('renders the <Form /> with populated data', () => {
-
-    const { container, getByText, getByPlaceholderText } = render(
+  it('renders the <Form /> with populated data', () => {
+    const { container, getByText, getByPlaceholderText } =render(
+      
       <Form>
         <Form.Title>Sign In Now</Form.Title>
-
-        <Form.Base>
+       <Form.Base>
           <Form.Input placeholder="Email address" onChange={() => {}} />
           <Form.Input type="password" placeholder="Password" onChange={() => {}} />
           <Form.Submit type="submit" disabled>
@@ -25,9 +24,8 @@ describe('<Form />', () => {
         <Form.TextSmall>
           This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
         </Form.TextSmall>
-      </Form>
+      </Form> 
     );
-
     expect(
       getByText("This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.")
     ).toBeTruthy();
@@ -40,7 +38,7 @@ describe('<Form />', () => {
   });
 
   it('renders the <Form /> with an error', () => {
-    const { container, getByText, queryByText } = render(
+    const { container, getByText, queryByText } =render(
       <Form>
         <Form.Error>Your email address is already being used</Form.Error>
         <Form.Submit type="submit">Sign In</Form.Submit>
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -6,25 +6,21 @@ import { HeaderContainer } from '../container/header';
 import { FooterContainer } from '../container/footer';
 import * as ROUTES from '../constants/routes';
 
-
 export default function SignIn() {
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
-const [emailAddress, setEmailAddress] = useState('');
-const [password, setPassword] = useState('');
-const [error, setError] = useState('');
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    // check form input elements are valid
+  const isInvalid = password === '' || emailAddress === '';
 
-    const isInvalid = password === '' || emailAddress === '';
-    
-    const handleSignIn = (event) => {
-        event.preventDefault();
+  const handleSignin = (event) => {
+    event.preventDefault();
 
-        //firebase work here.
-        return firebase 
-        .auth()
+    return firebase
+      .auth()
       .signInWithEmailAndPassword(emailAddress, password)
       .then(() => {
         history.push(ROUTES.BROWSE);
@@ -36,40 +32,40 @@ const [error, setError] = useState('');
       });
   };
 
-    // email & password. 
-
-    return (
-    <><HeaderContainer>
+  return (
+    <>
+      <HeaderContainer>
         <Form>
-            <Form.Title>Sign In</Form.Title>
-            {error && <Form.Error data-testid="error">{error}</Form.Error>}
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
 
-            <Form.Base onSubmit={handleSignIn} method="POST">
-                <Form.Input 
-                placeholder="Email address"
-                value={emailAddress}
-                onChange={({target}) => setEmailAddress(target.value)}
-                />
-                <Form.Input
-                placeholder="Password"
-                type="password"
-                autoComplete="off"
-                value={password}
-                onChange={({target}) => setPassword(target.value)}
-                />
-                <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
-                    Sign In
-                </Form.Submit>
-            </Form.Base>
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
 
-            <Form.Text>
-                New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
-            </Form.Text>
-            <Form.TextSmall>
-                This page is protected by Google reCAPTHA to ensure you're not a bot. Learn more. 
-            </Form.TextSmall>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
         </Form>
-        </HeaderContainer><FooterContainer />
-        </>
-    );
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
